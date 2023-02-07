939516572f52c9d151f7b1ede502f3191b601409
Jmin1701 <jmin1701@gmail.com>
2023-02-07 18:28:13 -0500

    Part 1-implemented drawing lines between vertices

--- generator/src/main/java/ca/mcmaster/cas/se2aa4/a2/generator/DotGen.java
+++ generator/src/main/java/ca/mcmaster/cas/se2aa4/a2/generator/DotGen.java
@@ -1,13 +1,17 @@
 package ca.mcmaster.cas.se2aa4.a2.generator;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 import java.util.Random;
 
 import ca.mcmaster.cas.se2aa4.a2.io.Structs.Vertex;
 import ca.mcmaster.cas.se2aa4.a2.io.Structs.Property;
+import ca.mcmaster.cas.se2aa4.a2.io.Structs.Segment;
 import ca.mcmaster.cas.se2aa4.a2.io.Structs.Mesh;
+import java.awt.Color;
 
 public class DotGen {
 
@@ -39,7 +43,61 @@ public class DotGen {
             verticesWithColors.add(colored);
         }
 
-        return Mesh.newBuilder().addAllVertices(verticesWithColors).build();
+        Set <Segment> segments = new HashSet<>();
+        for (Vertex v1: verticesWithColors){
+            for (Vertex v2: verticesWithColors){
+
+                //only draw segment if vertices are in either the same row or column
+                if ((v1.getX() == v2.getX() &&  v1.getY() != v2.getY() )|| (v1.getX() != v2.getX() &&  v1.getY() == v2.getY())) {
+
+                    Color v1Color = extractColor(v1.getPropertiesList());
+                    Color v2Color = extractColor(v2.getPropertiesList());
+
+                    //calculate average color and set as segment color
+                    int averageR = (v1Color.getRed() + v2Color.getRed())/2;
+                    int averageG = (v1Color.getGreen() + v2Color.getGreen())/2;
+                    int averageB = (v1Color.getBlue() + v2Color.getBlue())/2;
+                
+                    String segmentColor = averageR + "," + averageG + "," + averageB;
+                    String startVertex = v1.getX() + "," + v1.getY();
+                    String endVertex = v2.getX() + "," + v2.getY();
+
+                    //create and add properties(color, start & end vertices) to each line segment
+                    Property color = Property.newBuilder().setKey("rgb_color").setValue(segmentColor).build();
+                    Property startPos = Property.newBuilder().setKey("start_vertex").setValue(startVertex ).build();
+                    Property endPos = Property.newBuilder().setKey("end_vertex").setValue(endVertex).build();
+
+                    Segment newS = Segment.newBuilder().addProperties(color).build();
+                    newS = newS.toBuilder().addProperties(startPos).build();
+                    newS = newS.toBuilder().addProperties(endPos).build();
+
+                    segments.add(newS);
+                }
+            }
+        }
+
+        //generate mesh with colored vertices and line segments
+        Mesh mesh = Mesh.newBuilder().addAllVertices(verticesWithColors).build();
+        Mesh meshWithSegments = mesh.toBuilder().addAllSegments(segments).build();
+
+        return meshWithSegments; 
+    } 
+    
+    private Color extractColor(List<Property> properties) {
+        String val = null;
+        for(Property p: properties) {
+            if (p.getKey().equals("rgb_color")) {
+                val = p.getValue();
+            }
+        }
+        if (val == null)
+            return Color.BLACK;
+        String[] raw = val.split(",");
+        int red = Integer.parseInt(raw[0]);
+        int green = Integer.parseInt(raw[1]);
+        int blue = Integer.parseInt(raw[2]);
+        return new Color(red, green, blue);
     }
 
 }
+
--- visualizer/src/main/java/ca/mcmaster/cas/se2aa4/a2/visualizer/GraphicRenderer.java
+++ visualizer/src/main/java/ca/mcmaster/cas/se2aa4/a2/visualizer/GraphicRenderer.java
@@ -3,6 +3,7 @@ package ca.mcmaster.cas.se2aa4.a2.visualizer;
 import ca.mcmaster.cas.se2aa4.a2.io.Structs.Mesh;
 import ca.mcmaster.cas.se2aa4.a2.io.Structs.Vertex;
 import ca.mcmaster.cas.se2aa4.a2.io.Structs.Property;
+import ca.mcmaster.cas.se2aa4.a2.io.Structs.Segment;
 
 import java.awt.Graphics2D;
 import java.awt.Stroke;
@@ -10,6 +11,8 @@ import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.geom.Ellipse2D;
 import java.util.List;
+import java.awt.geom.Line2D;
+import java.awt.geom.Point2D;
 
 public class GraphicRenderer {
 
@@ -27,13 +30,46 @@ public class GraphicRenderer {
             canvas.fill(point);
             canvas.setColor(old);
         }
+
+        //iterate through and draw each line segment
+        for (Segment s : aMesh.getSegmentsList()) {
+            canvas.setColor(extractColor(s.getPropertiesList()));
+            String startVertex = null;
+            String endVertex = null;
+
+            for(Property p: s.getPropertiesList()) {
+                if (p.getKey().equals("start_vertex")) {
+                    startVertex = p.getValue();
+                }
+                if (p.getKey().equals("end_vertex")) {
+                    endVertex = p.getValue();
+                }
+            }
+            
+            String[] rawStart = startVertex.split(",");
+            double x_Start = Double.parseDouble(rawStart[0]);
+            double y_Start = Double.parseDouble(rawStart[1]);
+
+            String[] rawEnd = endVertex.split(",");
+            double x_End = Double.parseDouble(rawEnd[0]);
+            double y_End = Double.parseDouble(rawEnd[1]);
+
+            //set the start and end poitns of each line segment from the properties
+            Point2D startPoint = new Point2D.Double(x_Start,y_Start);
+            Point2D endPoint = new Point2D.Double(x_End,y_End);
+            
+            Line2D line = new Line2D.Double(startPoint,endPoint);
+
+            canvas.draw(line);
+            canvas.fill(line);
+        }
+        
     }
 
     private Color extractColor(List<Property> properties) {
         String val = null;
         for(Property p: properties) {
             if (p.getKey().equals("rgb_color")) {
-                System.out.println(p.getValue());
                 val = p.getValue();
             }
         }
