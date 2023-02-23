bdc973ac823c5c16bf8306764d725e19006e9035
michaelbaskaran <baskam1@mcmaster.ca>
2023-02-22 22:51:50 -0500

    removed meshfactory dump and updated the polymesh dump

--- visualizer/src/main/java/ca/mcmaster/cas/se2aa4/a2/visualizer/MeshDump.java
+++ visualizer/src/main/java/ca/mcmaster/cas/se2aa4/a2/visualizer/MeshDump.java
@@ -5,30 +5,29 @@ import ca.mcmaster.cas.se2aa4.a2.io.Structs.Mesh;
 import ca.mcmaster.cas.se2aa4.a2.io.Structs.Vertex;
 import ca.mcmaster.cas.se2aa4.a2.io.Structs.Property;
 import ca.team50.adt.PolyMesh;
+import ca.team50.adt.Polygons;
 
 import java.io.IOException;
 import java.util.List;
 
 public class MeshDump {
 
-    public void dump(String fileName) throws IOException {
-        MeshFactory factory = new MeshFactory();
-        Mesh aMesh = factory.read(fileName);
-        this.dump(aMesh);
-    }
-
     public void dump(PolyMesh aMesh) {
-        List<Vertex> vertices = aMesh.getVerticesList();
-        System.out.println("|Vertices| = " + vertices.size());
-        for (Vertex v : vertices){
-            StringBuffer line = new StringBuffer();
-            line.append(String.format("(%.2f,%.2f)",v.getX(), v.getY()));
-            line.append(" [");
-            for(Property p: v.getPropertiesList()){
-                line.append(String.format("%s -> %s, ", p.getKey(), p.getValue()));
+        PolyMesh<Polygons> mesh = aMesh;
+
+        for (Polygons poly : mesh){
+            List<Vertex> vertices = poly.getVerticesList();
+            System.out.println("|Vertices| = " + vertices.size());
+            for (Vertex v : vertices){
+                StringBuffer line = new StringBuffer();
+                line.append(String.format("(%.2f,%.2f)",v.getX(), v.getY()));
+                line.append(" [");
+                for(Property p: v.getPropertiesList()){
+                    line.append(String.format("%s -> %s, ", p.getKey(), p.getValue()));
+                }
+                line.append("]");
+                System.out.println(line);
             }
-            line.append("]");
-            System.out.println(line);
         }
     }
 }
