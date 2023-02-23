a35ba61fc28a40b99b5d238dd37e39e6f08ba038
Davis <davislenover02@gmail.com>
2023-02-23 13:41:47 -0500

    Updated Point geometry

--- generator/src/main/java/ca/team50/generation/RandomGen.java
+++ generator/src/main/java/ca/team50/generation/RandomGen.java
@@ -1,6 +1,9 @@
 package ca.team50.generation;
 
-import java.awt.*;
+import org.locationtech.jts.geom.Coordinate;
+import org.locationtech.jts.geom.GeometryFactory;
+import org.locationtech.jts.geom.Point;
+
 import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
@@ -8,7 +11,7 @@ import java.util.Random;
 public class RandomGen {
 
     /**
-     * Generates an array of Point objects, each containing a unique x and y position
+     * Generates an array of Point (JTS) objects, each containing a unique x and y position
      * @param maxX the maximum value x can be (0 to maxX) (both values are inclusive)
      * @param maxY the maximum value y can be (0 to maxY) (both values are inclusive)
      * @param numOfPoints the number of points to be generated
@@ -28,26 +31,32 @@ public class RandomGen {
             throw new IllegalArgumentException("Number of points to generate given maxX and maxY is too large");
         }
 
+        // Create GeometryFactory for creating new points
+        GeometryFactory factory = new GeometryFactory();
+
         // Create points array to store all points in
-        // Note this creates an array of 0,0 points
+        // Note this creates an array of null points
         Point[] pointArray = new Point[numOfPoints];
 
         Random randomGen = new Random();
 
         for (int index = 0; index < pointArray.length; index++) {
 
-            // Create a new point (defaults to 0,0)
-            Point newPoint = new Point();
+            // Create newPoint Point object
+            Point newPoint;
 
-            // Convert updated point array to list to for checking if objects exist within the array (do-while loop below)
+            // Convert updated point array to Arraylist to for checking if objects exist within the array (do-while loop below)
             List<Point> arrayAsList = Arrays.asList(pointArray);
 
             // Keep re-generating point position if a point with the position already exists
             do {
                 // Number between 0 and maxX (both inclusive)
-                newPoint.x = randomGen.nextInt(maxX+1);
+                int xPos = randomGen.nextInt(maxX+1);
                 // Number between 0 and maxY (both inclusive)
-                newPoint.y = randomGen.nextInt(maxY+1);
+                int yPos = randomGen.nextInt(maxY+1);
+
+                // Update point with newly generated coordinates
+                newPoint = factory.createPoint(new Coordinate(xPos,yPos));
 
             } while (arrayAsList.contains(newPoint));
 
