a6b0d9b1e3964a35b768531e5294dfc17808ff00
Davis <davislenover02@gmail.com>
2023-03-21 22:33:38 -0400

    Further implementation of island shapes

--- island/src/main/java/ca/team50/shapes/Circle.java
+++ island/src/main/java/ca/team50/shapes/Circle.java
@@ -6,19 +6,24 @@ import org.locationtech.jts.geom.Geometry;
 import org.locationtech.jts.geom.GeometryFactory;
 import org.locationtech.jts.geom.Point;
 
+// This class describes an abstract circle
 public class Circle implements IslandShape {
 
     private Geometry circle;
 
     public Circle(Structs.Vertex centerOfMesh, double radius) {
+        // Create a circle
         this.circle = generateShape(centerOfMesh,radius,64);
     }
 
     public boolean isVertexInside(Structs.Vertex vertexToCheck) {
+        // Create a geometry factory
         GeometryFactory geoFactory = new GeometryFactory();
 
+        // Convert the vertex to check into a Point that JTS can understand since the shape is in JTS format
         Point coordToCheck = geoFactory.createPoint(new Coordinate(vertexToCheck.getX(),vertexToCheck.getY()));
 
+        // Check if the point is within the Geometry using a built in JTS method
         return (this.circle.contains(coordToCheck));
     }
 
--- island/src/main/java/ca/team50/shapes/IslandShape.java
+++ island/src/main/java/ca/team50/shapes/IslandShape.java
@@ -10,36 +10,43 @@ public interface IslandShape {
 
     // Creates an abstract shape
     default Geometry generateShape(List<Structs.Vertex> vertexList) {
-        // Create the geometry factory use to create the output of .getDiagram()
+
         GeometryFactory geoFactory = new GeometryFactory();
 
-        // First extract data from Polygons instance
+        // First extract data from vertex list
         CoordinateList coordinateList = new CoordinateList();
 
         for (Structs.Vertex currentVertex : vertexList) {
             coordinateList.add(new Coordinate(currentVertex.getX(),currentVertex.getY()));
         }
 
-        // Close the loop at the end by adding the starting point again
+        // Close the loop (shape loop) at the end by adding the starting point again
         coordinateList.add(new Coordinate(vertexList.get(0).getX(),vertexList.get(0).getY()));
 
+        // Create geometry
         return geoFactory.createPolygon(coordinateList.toCoordinateArray());
     }
 
     // Creates a circle instead of an abstract shape
      default Geometry generateShape(Structs.Vertex center, double radius, int precision) {
-        // Create the geometry factory use to create the output of .getDiagram()
+
+        // Create shape factory
         GeometricShapeFactory shapeFactory = new GeometricShapeFactory();
 
+        // Set number of points around the circle
         shapeFactory.setNumPoints(precision);
 
+        // Set the location of the centre
         shapeFactory.setCentre(new Coordinate(center.getX(),center.getY()));
 
+        // Set the size of the circle
         shapeFactory.setSize(radius*2);
 
+        // Create the circle
         return shapeFactory.createCircle();
     }
 
+    // Method to detect if any given point (vertex) is inside the shape may vary
     boolean isVertexInside(Structs.Vertex vertexToCheck);
 
 }
