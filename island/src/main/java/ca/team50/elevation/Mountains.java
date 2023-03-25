16bcb616d9fb7c98cec86ab53f5815a51a19ed9f
Jmin1701 <jmin1701@gmail.com>
2023-03-25 18:15:36 -0400

    Altitude and altemeric profiles

--- island/src/main/java/ca/team50/elevation/Mountains.java
+++ island/src/main/java/ca/team50/elevation/Mountains.java
@@ -1,4 +1,30 @@
 package ca.team50.elevation;
 
+import ca.mcmaster.cas.se2aa4.a2.io.Structs;
+import ca.team50.adt.PolyMesh;
+import ca.team50.adt.Polygons;
+import ca.team50.shapes.Circle;
+import ca.team50.shapes.IslandShape;
+import org.locationtech.jts.geom.Coordinate;
+
+import java.util.ArrayList;
+
+import static ca.team50.elevation.Volcano.volcanoAltitude;
+
 public class Mountains {
+
+    public static void mountainAltitude(ArrayList<Polygons> islandPoly, double numOf, double topAltitude, double botAltitude, double slopeRadius ){
+
+       for (int i = 0; i < numOf; i++) {
+
+            int index = (int)(Math.random() * islandPoly.size());
+
+            Structs.Vertex peak = islandPoly.get(index).getCentroid();
+
+            volcanoAltitude(islandPoly, peak, topAltitude, botAltitude, slopeRadius, slopeRadius, 30 );
+
+       }
+    }
+
 }
+
--- island/src/main/java/ca/team50/elevation/Plains.java
+++ island/src/main/java/ca/team50/elevation/Plains.java
@@ -4,6 +4,8 @@ import ca.mcmaster.cas.se2aa4.a2.io.Structs;
 import ca.team50.adt.PolyMesh;
 import ca.team50.adt.Polygons;
 import ca.mcmaster.cas.se2aa4.a2.io.Structs.Vertex;
+import ca.team50.elevation.Plains;
+import ca.team50.generation.TestIsland;
 
 import java.util.ArrayList;
 
@@ -12,9 +14,11 @@ public class Plains {
     //later, overall base altitude of plains can be determined through command line, and to what degree altitudes fluctuates can also be user determined
     public static void plainsAltitude(PolyMesh<Polygons> mesh, double altitude, double fluctuation) {
 
+
         for (Polygons currPoly: mesh){
-             currPoly.changeElevation(String.valueOf((altitude - fluctuation) + (Math.random() * (((altitude + fluctuation) - (altitude - fluctuation)) + 1))));
+             currPoly.changeElevation(String.valueOf((altitude - fluctuation) + (Math.random() * (((altitude + fluctuation) - (altitude - fluctuation)) + 0.1))));
         }
+
 
     }
 
