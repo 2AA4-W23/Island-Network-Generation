5d81261839f54d4c5041fe4bd3cc7a8fae0434cc
Davis <davislenover02@gmail.com>
2023-03-25 21:03:15 -0400

    Updating CLI

--- island/src/main/java/ca/team50/specification/CLInterfaceIsland.java
+++ island/src/main/java/ca/team50/specification/CLInterfaceIsland.java
@@ -12,15 +12,20 @@ public class CLInterfaceIsland {
     private static final Option meshOutput = new Option("o", "output", true, "Specify the name of the output mesh file (as a string): <output>.mesh (MUST INCLUDE .mesh in name!)");
     private static final Option islandGenMode = new Option("m", "mode", true, "Specify the mode of island generation, specified values are: " + getEnumValues() + ", default: lagoon");
     private static final Option helpOpt = new Option("h", "help", false, "Display input help");
-    private static final Option aquifer = new Option("a", "aquifer", true, "Specify the number of aquifers to be generated: ");
-
-
+    private static final Option aquifer = new Option("a", "aquifer", true, "Specify the number of aquifers to be generated (as an integer)");
+    private static final Option lakes = new Option("l", "lakes", true, "Specify the maximum number of lakes to be generated (as an integer)");
+    private static final Option rivers = new Option("r", "rivers", true, "Specify the maximum number of rivers to be generated (as an integer)");
+    private static final Option seed = new Option("s", "seed", true, "Specify the seed of the generation (as a long value)");
+    // TODO SOIL AND BIOMES
 
     private ModeType islandMode;
     private String meshInputString;
     private String meshOutputString;
 
     private Integer numAquifers;
+    private int numLakes;
+    private int numRivers;
+    private long numSeed;
 
 
     public CLInterfaceIsland(String[] args) {
@@ -34,6 +39,9 @@ public class CLInterfaceIsland {
         options.addOption(islandGenMode);
         options.addOption(helpOpt);
         options.addOption(aquifer);
+        options.addOption(lakes);
+        options.addOption(rivers);
+        options.addOption(seed);
 
         try {
 
@@ -65,12 +73,14 @@ public class CLInterfaceIsland {
             this.meshOutputString = commandLine.getOptionValue(meshOutput);
 
             // Get number of aquifers to be generated if requested
-            String numAquifersString = commandLine.getOptionValue(aquifer);
-            if (numAquifersString != null) {
-                this.numAquifers = Integer.parseInt(numAquifersString);
-            } else {
-                this.numAquifers = 0; // default value if not specified
-            }
+            this.numAquifers = Integer.parseInt(commandLine.getOptionValue(aquifer, "0"));
+
+            // Lakes, Rivers and Seed
+            this.numLakes = Integer.parseInt(commandLine.getOptionValue(lakes,"0"));
+            this.numRivers = Integer.parseInt(commandLine.getOptionValue(rivers,"0"));
+            this.numSeed = Integer.parseInt(commandLine.getOptionValue(seed,"1234"));
+
+
 
         } catch (Exception e) {
             ExceptionHandler.handleException(new InvalidCommandFormatException("Failed to parse arguments. Were commands inputted correctly?"));
@@ -92,6 +102,16 @@ public class CLInterfaceIsland {
 
     public Integer getNumAquifers() { return  numAquifers;}
 
+    public Integer getNumLakes() {
+        return numLakes;
+    }
+    public Integer getNumRivers() {
+        return numRivers;
+    }
+    public Long getSeed() {
+        return numSeed;
+    }
+
 
     // Method to print generator command line input help
     public static void printHelp() {
@@ -103,6 +123,9 @@ public class CLInterfaceIsland {
         options.addOption(islandGenMode);
         options.addOption(helpOpt);
         options.addOption(aquifer);
+        options.addOption(lakes);
+        options.addOption(rivers);
+        options.addOption(seed);
 
 
         formatter.printHelp("-<short or -- for long command> <numerical or string argument if required>",options);
